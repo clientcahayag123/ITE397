b161f9829b2781bac7260a9661341a2375980885
unknown <clal.cahayag.coc@phinamed.com>
2025-10-06 00:22:56 +0800

    registration form

---
+++ modules/registration.php
@@ -0,0 +1,20 @@
+<h2>Personal Information Form</h2>
+
+<form method="POST" action="">
+    <label>First Name:</label><br>
+    <input type="text" name="firstname" required><br><br>
+
+    <label>Last Name:</label><br>
+    <input type="text" name="lastname" required><br><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <label>Phone Number:</label><br>
+    <input type="text" name="phone" required><br><br>
+
+    <label>Address:</label><br>
+    <input type="text" name="address" required><br><br>
+
+    <input type="submit" value="Submit">
+</form>
